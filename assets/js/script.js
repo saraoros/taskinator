de249f0f5878f4569a81f472ccb7b15077e1f3d4
Sara Gochez <saragochez13@gmail.com>
2022-01-14 19:31:59 -0800

    added form variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,14 @@
-var buttonEl = document.querySelector("#save-task");
-console.log(buttonEl);
+var formEl = document.querySelector("#task-form");
+var tasksToDoEl = document.querySelector("#tasks-to-do");
+// this function prevents the page from reloading non-stop.
+var createTaskHandler = function (event) {
+  event.preventDefault();
+
+  var listItemEl = document.createElement("li");
+  listItemEl.className = "task-item";
+  listItemEl.textContent = "This is a new task.";
+  tasksToDoEl.appendChild(listItemEl);
+};
+
+//bottom of JS
+formEl.addEventListener("submit", createTaskHandler);
