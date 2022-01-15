f1d200d1e989d62528b42595b89e3c319e123131
Sara Gochez <saragochez13@gmail.com>
2022-01-14 23:04:18 -0800

    add ability to update and delete tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+var taskIdCounter = 0;
+
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
@@ -22,6 +24,49 @@ var createTaskHandler = function (event) {
   tasksToDoEl.appendChild(listItemEl);
 };
 
+actionContainerEl.appendChild(deleteButtonEl);
+var createTaskEl = function(taskDataObj) {
+    
+    var createTaskActions = function(taskId) {
+        var actionContainerEl = document.createElement("div");
+        actionContainerEl.className = "task-actions";
+        // create edit button
+var editButtonEl = document.createElement("button");
+editButtonEl.textContent = "Edit";
+editButtonEl.className = "btn edit-btn";
+editButtonEl.setAttribute("data-task-id", taskId);
 
+return actionContainerEl;
+
+    };
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+  
+    // add task id as a custom attribute
+    listItemEl.setAttribute("data-task-id", taskIdCounter);
+  
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
+    listItemEl.appendChild(taskInfoEl);
+  
+    tasksToDoEl.appendChild(listItemEl);
+  
+    // increase task counter for next unique id
+    taskIdCounter++;
+  };
+  var taskActionsEl = createTaskActions(taskIdCounter);
+  console.log(taskActionsEl);
+  var taskActionsEl = createTaskActions(taskIdCounter);
+listItemEl.appendChild(taskActionsEl);
+var taskActionsEl = createTaskActions(taskIdCounter);
+listItemEl.appendChild(taskActionsEl);
+
+tasksToDoEl.appendChild(listItemEl);
+var taskActionsEl = createTaskActions(taskIdCounter);
+listItemEl.appendChild(taskActionsEl);
+
+tasksToDoEl.appendChild(listItemEl);
+tasksToDoEl.appendChild(listItemEl);
 //bottom
 formEl.addEventListener("submit", createTaskHandler);
